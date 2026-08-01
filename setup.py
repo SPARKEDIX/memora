3780ada245e7048c3836409bc26e8f37a5f9da8b
Kartik <kartiknaveen2007@gmail.com>
2026-08-01 18:36:43 +0530

    update

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setup(
     long_description=long_description,
     long_description_content_type="text/markdown",
     author="Kartik Sharma",
-    author_email="kartik@example.com",
+    author_email="kartiknaveen2007@gmail.com",
     url="https://github.com/SPARKEDIX/memora",
     packages=["memora"],
     package_dir={"memora": "memora"},
